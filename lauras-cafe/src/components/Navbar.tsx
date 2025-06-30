6ec124756fbbe47e466a4a0cc0d6721ba86b9f1b
sakikout <bedalfior@gmail.com>
2025-06-30 12:36:03 -0300

    Navbar adicionada
    Ainda em ajuste

--- lauras-cafe/src/components/Navbar.tsx
+++ lauras-cafe/src/components/Navbar.tsx
@@ -7,7 +7,38 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 function NavbarComponent() {
     return (
         <>
-        
+        <Navbar className="nav-custom shadow-sm" expand="lg" variant="light" fixed="top">
+        <Container>
+          <Navbar.Brand className="fs-4 fw-bold">
+            <img
+              src="./src/assets/logo.png"
+              width={160}
+              height={90}
+              className="d-inline-block align-top me-2"
+              alt="Logo Laura's Café"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            <Nav className="fs-5">
+              <Nav.Link className="mx-2 nav-item">
+                Início
+              </Nav.Link>
+              <Nav.Link className="mx-2 nav-item">
+                Sobre Nós
+              </Nav.Link>
+              <Nav.Link className="mx-2 nav-item">
+                Menu
+              </Nav.Link>
+              <Nav.Link className="mx-2">
+                <Button className="btn-custom">Contato </Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
         </>
     );
 
